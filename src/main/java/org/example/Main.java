3ac41c079c3376fd366f0058537abb1743360170
iswarya1707 <iswaryakiot99@gmail.com>
2024-12-29 21:39:49 +0530

    Init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Browser drivers\\chromedriver.exe");
+        System.out.println("Hello, World!");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.makemytrip.com/");
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println(currentUrl);
+    }
+}
